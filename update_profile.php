d904e219f5abb353c33207a4ff6ee4886917e71d
DALI <sika.dali@telecomnancy.net>
2023-12-28 00:21:36 +0100

    finish update-profile

--- update_profile.php
+++ update_profile.php
@@ -5,6 +5,46 @@ session_start();
 
 $user_id = $_SESSION['user_id'];
 
+if (isset($_POST['update_profile'])) {
+    $update_name = mysqli_real_escape_string($conn, $_POST['update_name']);
+    $update_email = mysqli_real_escape_string($conn, $_POST['update_email']);
+
+    mysqli_query($conn, "UPDATE `user_form` SET name = '$update_name', email = '$update_email' WHERE id = '$user_id'") or die('query failed');
+
+    $old_pass = $_POST['old_pass'];
+    $update_pass = mysqli_real_escape_string($conn, md5($_POST['update_pass']));
+    $new_pass = mysqli_real_escape_string($conn, md5($_POST['new_pass']));
+    $confirm_pass = mysqli_real_escape_string($conn, md5($_POST['confirm_pass']));
+
+    if (!empty($update_pass) || !empty($new_pass) || !empty($confirm_pass)) {
+        if ($update_pass != $old_pass) {
+            $message[] = "old password not matched !";
+        } elseif ($new_pass != $confirm_pass) {
+            $message[] = "confirm password not matched !";
+        } else {
+            mysqli_query($conn, "UPDATE `user_form` SET password = '$confirm_pass' WHERE id = '$user_id'") or die('query failed');
+            $message[] = "password updated successfully !";
+        }
+    }
+
+    $update_image = $_FILES['update_image']['name'];
+    $update_image_size = $_FILES['update_image']['size'];
+    $update_image_tmp_name = $_FILES['update_image']['tmp_name'];
+    $update_image_folder = 'uploaded_img/' . $update_image;
+
+    if (!empty($update_image)) {
+        if ($update_image_size > 2000000) {
+            $message[] = 'image is too large';
+        } else {
+            $image_update_query = mysqli_query($conn, "UPDATE `user_form` SET image = '$update_image' WHERE id = '$user_id'") or die('query failed');
+            if ($image_update_query) {
+                move_uploaded_file($update_image_tmp_name, $update_image_folder);
+            }
+            $message[] = 'image is updated successfully !';
+        }
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -34,6 +74,11 @@ $user_id = $_SESSION['user_id'];
             } else {
                 echo '<img src="uploaded_img/' . $fetch['image'] . '">';
             }
+            if (isset($message)) {
+                foreach ($message as $msg) {
+                    echo '<div class="message">' . $msg . '</div>';
+                }
+            }
             ?>
             <div class="flex">
                 <div class="inputBox">
@@ -45,7 +90,7 @@ $user_id = $_SESSION['user_id'];
                     <input type="file" name="update_image" accept="image/jpg, image/jpeg, image/png" class="box">
                 </div>
                 <div class="inputBox">
-                    <input type="hidden" name="old_name" value="<?php echo $fetch['password']; ?>">
+                    <input type="hidden" name="old_pass" value="<?php echo $fetch['password']; ?>">
                     <span>old password :</span>
                     <input type="password" name="update_pass" placeholder="enter previous password" class="box">
                     <span>new password :</span>
