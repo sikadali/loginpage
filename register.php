b214959bb1e048975b5fd9c4945a1d4ec93cd2a0
DALI <sika.dali@telecomnancy.net>
2023-12-27 08:47:40 +0100

    register page php done

--- register.php
+++ register.php
@@ -1,3 +1,44 @@
+<?php
+
+include 'config.php';
+
+if (isset($_POST['submit'])) {
+
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+    $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+    $image = $_FILES['image']['name'];
+    $image_size = $_FILES['image']['size'];
+    $image_tmp_name = $_FILES['image']['tmp_name'];
+    $image_folder = 'uploaded_img/' . $image;
+
+    $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email= '$email' AND name = '$name' AND password = '$pass'") or die('query failed');
+
+    if (mysqli_num_rows($select) > 0) {
+        $message[] = 'user already exists';
+    } else {
+        if ($pass != $cpass) {
+            $message[] = 'confirm password mot matched';
+        } elseif ($image_size > 2000000) {
+            $message[] = 'image size is too large!';
+        } else {
+            $insert = mysqli_query($conn, "INSERT INTO `user_form`(name, email, password, image) VALUES('$name', '$email', '$pass', '$image')") or die('query failed');
+
+            if ($insert) {
+                move_uploaded_file($image_tmp_name, $image_folder);
+                $message[] = 'iregistered successfully !';
+                header('location:login.php');
+            } else {
+                $message[] = 'iregistered failed !';
+
+            }
+        }
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,12 +55,19 @@
 
         <form action="" method="post" enctype="multipart/form-data">
             <h3>Register now</h3>
+            <?php
+            if (isset($message)) {
+                foreach ($message as $msg) {
+                    echo '<div class="message">' . $msg . '</div>';
+                }
+            }
+            ?>
             <input type="text" name="name" placeholder="enter username" class="box" required>
-            <input type="text" name="email" placeholder="enter email" class="box" required>
-            <input type="text" name="password" placeholder="enter password" class="box" required>
-            <input type="text" name="cpassword" placeholder="confirm password" class="box" required>
-            <input type="file" class="box" accept="image/jpg, image/jpeg, image/png">
-            <input type="submit" value="register now" class="btn">
+            <input type="email" name="email" placeholder="enter email" class="box" required>
+            <input type="password" name="password" placeholder="enter password" class="box" required>
+            <input type="password" name="cpassword" placeholder="confirm password" class="box" required>
+            <input type="file" name="image" class="box" accept="image/jpg, image/jpeg, image/png">
+            <input type="submit" name="submit" value="register now" class="btn">
             <p>already have an account ? <a href="login.php">login now</a></p>
         </form>
 
