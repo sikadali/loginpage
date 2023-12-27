83ef60bdc37f4125f3cec30250e2e121acbfb857
DALI <sika.dali@telecomnancy.net>
2023-12-27 08:15:22 +0100

    html css for register page

--- register.php
+++ register.php
@@ -13,7 +13,14 @@
     <div class="form-container">
 
         <form action="" method="post" enctype="multipart/form-data">
+            <h3>Register now</h3>
             <input type="text" name="name" placeholder="enter username" class="box" required>
+            <input type="text" name="email" placeholder="enter email" class="box" required>
+            <input type="text" name="password" placeholder="enter password" class="box" required>
+            <input type="text" name="cpassword" placeholder="confirm password" class="box" required>
+            <input type="file" class="box" accept="image/jpg, image/jpeg, image/png">
+            <input type="submit" value="register now" class="btn">
+            <p>already have an account ? <a href="login.php">login now</a></p>
         </form>
 
     </div>
